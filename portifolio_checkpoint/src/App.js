31d7d94d4cba1a84493b01e72dfb4109de4bf842
William Lima <wilima010@gmail.com>
2021-11-16 21:55:38 -0300

    sdf

--- portifolio_checkpoint/src/App.js
+++ portifolio_checkpoint/src/App.js
@@ -1,12 +1,12 @@
-import Header from "./components/header/Header";
-import Parallax from "./components/parallax/Parallax";
+
+import Header from "./components/header/Header"
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Parallax/>
+      
     </div>
   );
 }
--- portifolio_checkpoint/src/components/header/Header.js
+++ portifolio_checkpoint/src/components/header/Header.js
@@ -1,17 +1,19 @@
 function Header () {
 
     return (
-        <div className= "container" id = "root">
-    <header className="d-flex justify-content-center py-3">
-      <ul className="nav nav-pills">
-        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
-      </ul>
-    </header>
-  </div>
+       <div>            
+            <div className = "headerConteiner">
+                <a className = "styles.logo" href = "">
+                    <img  src = "https://imagensemoldes.com.br/wp-content/uploads/2018/05/Sonic-Novo-Sonic-e-Amigos-PNG.png" alt = "sonic"/>
+                </a>
+                <ul className = "styles.menuList">
+                    <li><a>Menu</a></li>
+                    <li><a>Menu</a></li>
+                    <li><a>Menu</a></li>
+                    <li><a>Menu</a></li>
+                </ul>
+            </div>             
+       </div>
     )
 }
 
